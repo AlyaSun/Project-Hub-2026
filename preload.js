f26e6d4e0a5615ee8340f2cd1fc04f5a991c3d41
AlyaSun <102125403+AlyaSun@users.noreply.github.com>
2026-06-16 10:10:32 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -5,6 +5,11 @@ contextBridge.exposeInMainWorld('electronAPI', {
   toggleAlwaysOnTop: (pinned) => ipcRenderer.send('toggle-always-on-top', pinned),
   toggleOverlay: () => ipcRenderer.invoke('toggle-overlay'),
   showMainWindow: () => ipcRenderer.send('show-main-window'),
-  taskDataUpdated: () => ipcRenderer.send('task-data-updated'),
-  onTaskDataChanged: (callback) => ipcRenderer.on('task-data-changed', (_, data) => callback(data))
+  taskDataUpdated: (jsonData) => ipcRenderer.send('task-data-updated', jsonData),
+  onTaskDataChanged: (callback) => ipcRenderer.on('task-data-changed', (_, data) => callback(data)),
+  markTaskDone: (taskId) => ipcRenderer.invoke('mark-task-done', taskId),
+  // Backup / Restore
+  backupToOnedrive: () => ipcRenderer.invoke('backup-to-onedrive'),
+  restoreFromOnedrive: () => ipcRenderer.invoke('restore-from-onedrive'),
+  getBackupStatus: () => ipcRenderer.invoke('get-backup-status'),
 });
